cac353f6a6daab9a984f770e739b183d9836fdd4
uyplayer <32848979@qq.com>
2024-07-13 18:15:46 +0800

    add icon

--- main.cpp
+++ main.cpp
@@ -8,9 +8,6 @@
 
 int main(int argc, char* argv[])
 {
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
-
     QApplication app(argc, argv);
     mainWindow mainWindow;
     mainWindow.show();
